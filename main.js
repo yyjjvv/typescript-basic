14fdebd103f213f901819c33b4516378ddc1d5c9
yyjjvv <jiwonyoo66@gmail.com>
2023-12-06 22:41:13 +0900

    first quiz

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log('ts');
+let monsterName;
+let level;
+let hasGold;
+let skill;
+let area;
+monsterName = '코드잇 고블린';
+level = 255;
+hasGold = false;
+skill = undefined;
+area = null;
+console.log(`${monsterName}의 레벨은 ${level}이고,\n` +
+    `${hasGold ? '해치우면 골드를 얻는' : '해치워도 골드를 주지 않는'} 몬스터입니다.\n` +
+    `${skill ? `가진 능력은 ${skill} 이고 \n` : ''}` +
+    `${area ? `출현 지역은 ${area} 입니다.` : ''}`);
--- main.ts
+++ main.ts
@@ -1,2 +1,18 @@
-let size: number = 100;
-// size = 'L';
+let monsterName: string;
+let level: number;
+let hasGold: boolean;
+let skill: undefined;
+let area: null;
+
+monsterName = '코드잇 고블린';
+level = 255;
+hasGold = false;
+skill = undefined;
+area = null;
+
+console.log(
+  `${monsterName}의 레벨은 ${level}이고,\n` +
+    `${hasGold ? '해치우면 골드를 얻는' : '해치워도 골드를 주지 않는'} 몬스터입니다.\n` +
+    `${skill ? `가진 능력은 ${skill} 이고 \n` : ''}` +
+    `${area ? `출현 지역은 ${area} 입니다.` : ''}`
+);
